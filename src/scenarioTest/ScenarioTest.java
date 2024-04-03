421d95470cfe0b23f8e49e3ccd1a71cb0884e268
loic-de-tlse3 <loic.francois1@univ-tlse3>
2024-04-03 15:21:12 +0200

    Fin essai 2

--- src/scenarioTest/ScenarioTest.java
+++ src/scenarioTest/ScenarioTest.java
@@ -2,10 +2,13 @@ package scenarioTest;
 
 import villagegaulois.IEtal;
 import produit.Produit;
+import produit.Sanglier;
+import personnages.Gaulois;
 
 public class ScenarioTest {
 	public static void main(String[] args) {
-		IEtal<Produit>[] marche = new IEtal[3];
+		IEtal<Sanglier>[] marche = new IEtal[3];
+		marche[0].occuperEtal(new Gaulois("Bonnemine", 3), new Sanglier("Obelix", 5), 1);
 	}
 	
 }
